ccb63cff8c1df629a4dec0a26fd3ca64706bf210
Mark McArthey <mcarthey@gmail.com>
2024-07-30 21:18:35 -0500

    very inefficient cactus harvesting

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 dependencies = [
     (Items.Carrot_Seed, [(Items.Wood, 7), (Items.Hay, 7)]),
     (Items.Pumpkin_Seed, [(Items.Carrot, 3)]),
-    (Items.Sunflower_Seed, [(Items.Carrot, 1)]) 
+    (Items.Sunflower_Seed, [(Items.Carrot, 1)]),
+    (Items.Cactus_Seed, [(Items.Wood, 5), (Items.Hay, 5)]) 
 ]
 
 priorities = [
@@ -10,11 +11,15 @@ priorities = [
     (Items.Carrot, 6000),
     (Items.Pumpkin, 6000),
     (Items.Wood, 6000),
+    (Items.Cactus, 6000),
     (Items.Power, 100)
 ]
 
 # Track sunflower positions and petals
 sunflower_positions = {}
+
+# Track cactus positions and sizes
+cactus_positions = {}
 
 # Main management loop
 def manage_farm():
@@ -43,8 +48,14 @@ def handle_harvest():
                 if sunflower_positions[pos_key] > max_petals:
                     max_petals = sunflower_positions[pos_key]
             harvest_all_sunflowers_with_max_petals(max_petals)
+    elif get_entity_type() == Entities.Cactus:
+        sort_cacti()
+        if check_sorted_cacti():
+            harvest_all_cacti()
     else:
         harvest()
+
+    # plant something new in the harvested position   
     do_planting(pos)
 
 def all_petals_known():
@@ -72,6 +83,29 @@ def harvest_all_sunflowers_with_max_petals(max_petals):
         sunflower_positions.pop(key)
         do_planting(key) # Plant something new in the harvested position
 
+def harvest_all_cacti():
+    quick_print("Harvesting all cacti.")
+    harvest()
+    # Clear the cactus_positions dictionary manually
+    for key in list(cactus_positions):
+        cactus_positions.pop(key)
+
+def sort_cacti():
+    sort_rows()
+    sort_columns()
+
+def check_sorted_cacti():
+    world_size = get_world_size()
+    for x in range(world_size):
+        for y in range(world_size):
+            current_size = measure_at(x, y)
+            if (y + 1 < world_size and measure_at(x, y + 1) < current_size or
+                x + 1 < world_size and measure_at(x + 1, y) < current_size or
+                y - 1 >= 0 and measure_at(x, y - 1) > current_size or
+                x - 1 >= 0 and measure_at(x - 1, y) > current_size):
+                return False
+    return True
+  
 # Planting functions
 def do_planting(position):
     # Move to the specified position
@@ -81,7 +115,7 @@ def do_planting(position):
     for priority in priorities:
         item, target = priority
         if num_items(item) < target:
-            plant_based_on_priority(item)
+            plant_based_on_priority(item, position)
             return
 
     # If no priorities, determine the most deficient item
@@ -90,8 +124,9 @@ def do_planting(position):
     carrot_count = num_items(Items.Carrot)
     pumpkin_count = num_items(Items.Pumpkin)
     power_count = num_items(Items.Power)
+    cactus_count = num_items(Items.Cactus)
 
-    min_count = min(wood_count, hay_count, carrot_count, pumpkin_count, power_count)
+    min_count = min(wood_count, hay_count, carrot_count, pumpkin_count, power_count, cactus_count)
     if min_count == wood_count:
         plant_tree_or_bush()
     elif min_count == hay_count:
@@ -102,8 +137,10 @@ def do_planting(position):
         plant_pumpkin()
     elif min_count == power_count:
         plant_sunflower()
+    elif min_count == cactus_count:
+        plant_cactus(position)
 
-def plant_based_on_priority(item):
+def plant_based_on_priority(item, position):
     if item == Items.Wood:
         plant_tree_or_bush()
     elif item == Items.Hay:
@@ -112,6 +149,8 @@ def plant_based_on_priority(item):
         plant_carrot()
     elif item == Items.Pumpkin:
         plant_pumpkin()
+    elif item == Items.Cactus:
+        plant_cactus(position)
     elif item == Items.Power:
         plant_sunflower()
 
@@ -150,6 +189,11 @@ def update_sunflower_positions():
     petals = measure()
     sunflower_positions[pos] = petals
 
+def update_cactus_positions(position):
+    pos = (get_pos_x(), get_pos_y())
+    size = measure()
+    cactus_positions[pos] = size
+
 def plant_pumpkin():
     if num_items(Items.Pumpkin_Seed) == 0:
         ensure_resources_for(Items.Pumpkin_Seed)
@@ -172,10 +216,18 @@ def plant_grass():
     prepare_ground()
     plant(Entities.Grass)
 
+def plant_cactus(position):
+    if num_items(Items.Cactus_Seed) == 0:
+        ensure_resources_for(Items.Cactus_Seed)
+        trade(Items.Cactus_Seed)
+    prepare_ground_for_planting()
+    plant(Entities.Cactus)
+    update_cactus_positions(position)
+
 # Helper functions
 def something_growing():
     entity = get_entity_type()
-    return entity in [Entities.Bush, Entities.Carrots, Entities.Grass, Entities.Pumpkin, Entities.Tree, Entities.Sunflower]
+    return entity in [Entities.Bush, Entities.Carrots, Entities.Grass, Entities.Pumpkin, Entities.Tree, Entities.Sunflower, Entities.Cactus]
 
 def have_enough(item, amount):
     return num_items(item) >= amount
@@ -212,5 +264,37 @@ def prepare_ground():
         till()
     # No need to water soil for bushes or grass
 
+def sort_rows():
+    world_size = get_world_size()
+    for y in range(world_size):
+        sorted_row = False
+        while not sorted_row:
+            sorted_row = True
+            for x in range(world_size - 1):
+                current_size = measure_at(x, y)
+                next_size = measure_at(x + 1, y)
+                if current_size > next_size:
+                    move_to(x, y)
+                    swap(East)
+                    sorted_row = False
+
+def sort_columns():
+    world_size = get_world_size()
+    for x in range(world_size):
+        sorted_column = False
+        while not sorted_column:
+            sorted_column = True
+            for y in range(world_size - 1):
+                current_size = measure_at(x, y)
+                next_size = measure_at(x, y + 1)
+                if current_size > next_size:
+                    move_to(x, y)
+                    swap(North)
+                    sorted_column = False
+
+def measure_at(x, y):
+    move_to(x, y)
+    return measure()
+           
 # Start managing the farm
 manage_farm()
